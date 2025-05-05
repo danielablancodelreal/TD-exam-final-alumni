6143b6f95edde41ab86b84fdb7f03c49c9f02ea0
Daniela Blanco del Real <daniela.blancodelreal.practicas@telefonica.com>
2025-05-05 08:16:01 +0200

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("../../icai2025-5f9879654f17.json")
+  credentials = file("../../lab5-458007-bdf939040eeb.json")
 
   project = var.gcp-project
   region  = var.gcp-region
