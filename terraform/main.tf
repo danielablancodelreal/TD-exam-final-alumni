067fe7b9db79bf9ebcb6519a31bdae003e9f701c
Daniela Blanco del Real <daniela.blancodelreal.practicas@telefonica.com>
2025-05-05 08:15:01 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "primary" {
-  name     = 
-  location = 
+  name     = var.gcp-cluster-name
+  location = var.gcp-zone
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -13,11 +13,11 @@ resource "google_container_cluster" "primary" {
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${var.gcp-cluster-name}-node-pool"
   cluster    = google_container_cluster.primary.name
-  location   = 
-  node_count = 
+  location   = var.gcp-zone
+  node_count = var.gcp-node-count
 
   node_config {
-    machine_type = 
+    machine_type = e2.small
     disk_size_gb = "20"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
